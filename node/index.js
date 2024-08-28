d6eb8be75b4deffa1014165515921cb85fd6643c
Pedro Braga <phbm.braga@gmail.com>
2024-08-28 10:07:58 -0300

    adding morgan to log requests

--- node/index.js
+++ node/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
+const morgan = require('morgan')
 const express = require('express');
 const sequelize = require('./config/database');
 const routes = require('./routes/index.js');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json());
-routes(app)
+routes(app);
 const PORT = process.env.PORT || 3000
 
 sequelize.sync();
